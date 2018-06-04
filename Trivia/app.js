026095b5f41a33838807361a77224bd52f24130a
Vinicia Lara <39544010+ViniciaLaraRangel@users.noreply.github.com>
2018-06-04 00:39:32 -0500

    Add files via upload

---
+++ Trivia/app.js
@@ -0,0 +1,12 @@
+var saludo = document.getElementById("saludar");
+var nombre = document.getElementById("userName").value;
+
+function conseguirNombre () {
+    var usuario = document.getElementById("userName").innerHTML = "persona";  
+    persona.innerHTML = "Bienvenido " + "nombre";
+}
+
+
+
+
+
